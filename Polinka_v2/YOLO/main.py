f25b02e0b86d623e869a2b5b3d47f50b95036840
Czaplaii <czapla.jakub2@gmail.com>
2026-03-29 15:23:32 +0200

    Yolo models
    3 gotowe modele YOLO,
    plik .py z inference
    1 obraz testowy
    1 przykładowy rezultat z modelu

--- Polinka_v2/YOLO/main.py
+++ Polinka_v2/YOLO/main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from ultralytics import YOLO
+import cv2
+"""
+test -> dataset ze zdjęciami, można też przetestować mp4
+results -> zwraca zawartość test z nałożonymi boxami
+"""
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# załaduj model
+#model = YOLO("yolov8n.pt") #szybki, ale mało dokładny
+model = YOLO("yolov8m.pt") #średniak
+#model = YOLO("yolov8x.pt") #wolny, ale dokładny
+"""
+# info o modelu
+model.info()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#trening basic
+results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+"""
+
+#show
+#model("test/chair.jpg", show=True)
+#input("type anything...")
+results = model("test/chair.jpg")
+
+#zapisz wynik do results
+i = 0
+for r in results:
+    i+1
+    im = r.plot()
+    cv2.imwrite(f"results/result_{i}.jpg",im)
